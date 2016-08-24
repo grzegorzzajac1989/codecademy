e01b7e17b7bf63ad6c88794f1d0754101621cadb
grzegorzzajac1989 <zajacgrzegorz1989@gmail.com>
2016-08-24 20:39:04 +0200

    Add codecademy JS/Ruby solutions

---
+++ Ruby/Histogram.rb
@@ -0,0 +1,16 @@
+=begin
+	In this project, we'll build a program that takes a user's input, 
+	then builds a hash from that input. Each key in the hash will be a word from the user; 
+	each value will be the number of times that word occurs.
+=end
+
+
+puts "Text please: "
+text = gets.chomp
+
+words = text.split(" ")
+frequencies = Hash.new(0)
+words.each { |word| frequencies[word] += 1 }
+frequencies = frequencies.sort_by { |a, b| b } 
+frequencies.reverse!
+frequencies.each { |word, count| puts word + " " + count.to_s }
