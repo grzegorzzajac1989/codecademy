67e5bbd09b5f89c64bf04d4a0900e27f4f8668ec
grzegorzzajac1989 <zajacgrzegorz1989@gmail.com>
2016-09-01 08:52:28 +0200

    finish path

--- ruby/sort_array.rb
+++ ruby/sort_array.rb
@@ -1,5 +1,6 @@
 my_array = ["raindrops", :kettles, "whiskers", :mittens, :packages, 1, 2]
 
-symbol_filter = lambda {|x| x.is_a? Integer }
+puts my_array.select {|a| a.is_a? String}
 
-my_array.select(&symbol_filter)
+symbol_filter = lambda {|x| x.is_a? Integer }
+puts my_array.select(&symbol_filter)
